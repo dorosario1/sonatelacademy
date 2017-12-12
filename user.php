f1335d4265507c0b552d1c40ef9967eaaf3ddcfb
dorosario1 <34451824+dorosario1@users.noreply.github.com>
2017-12-12 10:01:54 +0000

    Add files via upload

---
+++ user.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+
+    <title></title>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+<a href="calcul.php">Calculatrice</a><br>
+<a href="dateheure.php">Date</a>
+</body>
